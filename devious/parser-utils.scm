86e79410bf6ac9df27363816e0a8fae09302beb6
Aeron Paul Sioson <theblacksquid@users.noreply.github.com>
2016-03-11 02:47:40 -0800

    rework replace-split to account for #\space

--- devious/parser-utils.scm
+++ devious/parser-utils.scm
@@ -1,7 +1,5 @@
 (define replace-split
     (lambda (str rep sub) 
-    ; replace-split: a procedure that takes str (type: string) and 
-    ; returning a list with all instances of rep is replaced by sub.
     (begin
         ; ls: the internal representation fo the string
         ; being processed. I don't like using let
@@ -11,25 +9,25 @@
         (if (eqv? '() (cdr ls))
             ; return the resulting list if EOL is reached
             ls
-            (if (eqv? (car ls) rep)
-                ; replace the first item with sub
-                (cons 
-                    sub
-                    (replace-split 
-                        (list->string (cdr ls))
-                        rep
-                        sub)
+            (cond  
+                ((eqv? (car ls) #\space) 
+                    (replace-split (list->string (cdr ls)) rep sub) 
                 )
-                ; skip char not matching rep
-                (cons 
-                    (car ls)
-                    (replace-split 
-                        (list->string (cdr ls))
-                        rep
-                        sub)
+                ((eqv? (car ls) rep) 
+                    (cons 
+                        sub
+                        (replace-split (list->string (cdr ls)) rep sub)
+                    )
+                )
+                (else 
+                    (cons 
+                        (car ls)
+                        (replace-split (list->string (cdr ls)) rep sub)
+                    )
                 )
             )
         )
-    )   
     )
+    )   
 )
+
