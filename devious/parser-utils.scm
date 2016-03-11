bec37dd40b2efa876842f252f8232c1ef34ea0d5
Aeron Paul Sioson <theblacksquid@users.noreply.github.com>
2016-03-11 01:30:00 -0800

    Create parser-utils.scm
    encountered a bug in biwascheme where the last char in ls would replace everything but the chars replaced with sub. reported bug at main repo.

---
+++ devious/parser-utils.scm
@@ -0,0 +1,33 @@
+(define replace-split
+    (lambda (str rep sub) 
+    (begin
+        ; ls: the internal representation fo the string
+        ; being processed. I don't like using let
+        ; simplicity is beauty
+        (define ls (string->list str))
+        ; check for EOL
+        (if (eqv? '() (cdr ls))
+            ; return the resulting list if EOL is reached
+            ls
+            (if (eqv? (car ls) rep)
+                ; replace the first item with sub
+                (cons 
+                    sub
+                    (replace-split 
+                        (list->string (cdr ls))
+                        rep
+                        sub)
+                )
+                ; skip char not matching rep
+                (cons 
+                    (car ls)
+                    (replace-split 
+                        (list->string (cdr ls))
+                        rep
+                        sub)
+                )
+            )
+        )
+    )   
+    )
+)
